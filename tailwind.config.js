6e7c278a40534308b3e45358044227bfe32856b3
bdfpplinsinga <daniel@burovoordeboeg.nl>
2022-08-10 07:00:49 +0200

    project 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ['./src/**/*.njk', './src/**/*.md',],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'violet': 'hsl(257, 40%, 49%)',
+        'magenta': 'hsl(300, 69%, 71%)',
+      },
+      fontFamily: {
+        'sans': ['Open Sans'],
+        'headings': ['Poppins'],
+      }
+    },
   },
   plugins: [],
 }

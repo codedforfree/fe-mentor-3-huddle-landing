b5e5b04a284767ff98cedc4d39fb0fcd499bf851
bdfpplinsinga <daniel@burovoordeboeg.nl>
2022-08-10 10:27:43 +0200

    add gsap

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,28 @@ import Alpine from 'alpinejs'
 window.Alpine = Alpine
 
 Alpine.start()
+
+
+// GSAP
+
+let tl = gsap.timeline({ defaults: { duration: .8 } })
+
+
+// Svg
+tl.from("svg", {
+    opacity: 0,
+    xPercent: -100,
+})
+    .from("svg .tablet", {
+        opacity: 0,
+        x: -100
+    })
+    // Content
+    .from(".js-content", {
+        opacity: 0,
+        x: 200,
+    }, "-=0.8")
+
+    .from("header, footer", {
+        opacity: 0,
+    })
